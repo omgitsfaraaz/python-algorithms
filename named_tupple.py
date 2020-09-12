4e16c838cc45615dc429fe6c1edb9a22ba52f562
Mohsin <mohsinsait23@gmail.com>
2020-09-12 12:39:07 +0530

    namedtuple

---
+++ named_tupple.py
@@ -0,0 +1,7 @@
+from collections import namedtuple
+n = int(input())
+data = namedtupple("data", input().split())
+sum = 0
+for i in range(n):
+	sum += int(data(*input().split()).MARKS)
+print(sum / n)
