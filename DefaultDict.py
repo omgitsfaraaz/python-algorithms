aa5b7af59b9623f5ea624162d69dc61604c81b49
unknown <mohsinsait23@gmail.com>
2020-10-17 22:29:50 +0530

    DefaultDict Tutorial

---
+++ DefaultDict.py
@@ -0,0 +1,18 @@
+from collections import defaultdict 
+
+nm = list(map(int, input().split(' ')))
+n, m = nm[0], nm[1]
+d = defaultdict(list)
+l = []
+
+for i in range(1, n + 1):
+	d[input()].append(i)
+
+for i in range(m):
+	l.append(input())
+
+for k in l:
+	if k in d.keys():
+		print(*d[k])
+	else:
+		print(-1)
